263f5d458af3b254218303a7047ba5256a5d045d
Lavinia Tinca <lavinia_tinca@yahoo.com>
2023-02-22 22:53:58 +0200

    Add project files.

---
+++ StringConsoleApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using StringConsoleAppLibrary;
+
+using System;
+using System.Collections.Generic;
+
+namespace StringAppProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the StringConsoleApp");
+            Console.WriteLine("============================\n");
+            StringConsoleApp stringConsoleApp = new StringConsoleApp();
+            runMenu();
+
+            void runMenu()
+            {
+                bool endApp = false;
+                while (!endApp)
+                {
+                    DisplayMenu();
+                    Console.WriteLine("==== Select an option: ==== \n");
+                    string option = "";
+                    option = Console.ReadLine();                  
+                    stringConsoleApp.Operation(option);
+
+                    Console.WriteLine("------------------------\n");
+                    if (option == "x") endApp = true;
+
+                    Console.WriteLine("\n"); // Friendly linespacing.
+                }
+            }
+            
+
+            static void DisplayMenu()
+            {
+                
+                Console.WriteLine("==== Select an option: ==== \n");
+                Console.WriteLine("a - Convert a string to uppercase");
+                Console.WriteLine("b - Reverse a string");
+                Console.WriteLine("c - Count the number of vowels in a string");
+                Console.WriteLine("d - Count the number of words in a string");
+                Console.WriteLine("e - Convert a string to title case");
+
+                Console.WriteLine("x - Quit");
+            }
+		}
+    }
+}
---
+++ StringConsoleAppLibrary/StringConsoleAppLibrary.cs
@@ -0,0 +1,132 @@
+﻿using System.Diagnostics;
+using System.Globalization;
+
+namespace StringConsoleAppLibrary
+{
+    public class StringConsoleApp
+    {
+        public StringConsoleApp()
+        {
+            StreamWriter logFile = File.CreateText("stringConsoleApp.log");
+            Trace.Listeners.Add(new TextWriterTraceListener(logFile));
+            Trace.AutoFlush = true;
+            Trace.WriteLine("Starting StringConsoleApp Log");
+            Trace.WriteLine(String.Format("Started {0}", System.DateTime.Now.ToString()));
+        }
+
+        public string Operation(string option)
+        {
+            string result = "";
+            int res = 0;
+            switch (option)
+            {
+                case "a":
+                    result = convertStringToUppercase();
+                    Console.WriteLine(result);  
+                    break;
+                case "b":
+                    result = reverseAString();
+                    Console.WriteLine(result);
+                    break;
+                case "c":
+                    res = numberOfVowels();
+                    Console.WriteLine(res);
+                    Trace.WriteLine(String.Format("the given string has {0} vowels!", res));
+                    break;
+                case "d":
+                    //Count the number of words in a string
+                    res = numberOfWords();
+                    Console.WriteLine(res);
+                    Trace.WriteLine(String.Format("the given string has {0} words!", res));
+                    break;
+                case "e":
+                    // Convert a string to title case
+                    string s = ReadString();
+                    result = convertStringToTitleCase(s);
+                    //Trace.WriteLine(String.Format("The string {0} converted to Title Case is: {1} ", s, result));
+                    Console.WriteLine(result);
+                    break;
+                case "x":
+                    break;
+                // Return text for an incorrect option entry.
+                default:
+                    Console.WriteLine("Incorrect Option. Please retry!");
+                    break;
+            }
+            return result;
+        }
+
+        static string ReadString()
+        {
+            Console.WriteLine("Type a string, and then press Enter: ");
+            string result = "";
+            result = Console.ReadLine();
+            return result;
+        }
+
+        string convertStringToUppercase()
+        {
+            string s = ReadString();
+           
+            return s.ToUpper();
+        }
+
+        string reverseAString()
+        {
+            string s = ReadString();
+            string reversedString = String.Empty;
+            char[] array = s.ToCharArray();
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                reversedString += array[i];
+            }
+
+            //return (string) s.Reverse();
+            return reversedString;
+        }
+
+        int numberOfVowels()
+        {
+            string s = ReadString();
+            int numberOfVowels = 0;
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+            for (int i = 0; i < s.Length; i++)
+            {
+                foreach (var item in vowels)
+                {
+                    if (s[i] == item)
+                    {
+                        numberOfVowels++;
+                    }
+                }
+            }
+            return numberOfVowels;
+        }
+
+        int numberOfWords()
+        {
+            string s = ReadString();
+            int numberOfWords = 0;
+            for (int i = 0; i < s.Length-1; i++)
+            {
+                if ((s[i] == ' ') & (s[i+1] == ' '))
+                {
+                    i++;
+                }
+                else if ((i!=0) & (s[i] == ' '))
+                {
+                    numberOfWords++;
+                }                        
+            }
+            numberOfWords++;
+            return numberOfWords;
+        }
+
+        string convertStringToTitleCase(string s)
+        {
+            TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
+
+            return textInfo.ToTitleCase(s);
+        }
+    }
+}
